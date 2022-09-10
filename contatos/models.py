9ade19b7c900681b72d142df5428130516950fa1
Rafael-Mota-Moraes <rafaelmota622@gmail.com>
2022-09-10 11:14:14 -0300

    condicionais

--- contatos/models.py
+++ contatos/models.py
@@ -3,13 +3,14 @@ from django.utils import timezone
 
 # Create your models here.
 
+
 class Categoria(models.Model):
     nome = models.CharField(max_length=255)
-    
+
     def __str__(self) -> str:
         return self.nome
-    
-    
+
+
 class Contato(models.Model):
     nome = models.CharField(max_length=255)
     sobrenome = models.CharField(max_length=255, blank=True)
@@ -18,6 +19,7 @@ class Contato(models.Model):
     data_criacao = models.DateTimeField(default=timezone.now)
     descricao = models.TextField(blank=True)
     categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
+    mostrar = models.BooleanField(default=True)
 
     def __str__(self) -> str:
         return self.nome
