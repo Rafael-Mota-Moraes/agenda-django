2b4af01a0d2bdffcc72d3e1a1a20176e139fa6f4
Rafael Mota Moraes <rafaelmota622@gmail.com>
2024-05-16 12:11:58 -0300

    Manipulando QuerySets Django com filter, order_by e fatiamento

--- contact/admin.py
+++ contact/admin.py
@@ -4,11 +4,11 @@ from contact import models
 
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    list_display = 'id', 'first_name', 'last_name', 'phone',
+    list_display = 'id', 'first_name', 'last_name', 'phone', 'show',
     ordering = '-id',
     search_fields = 'id', 'first_name', 'last_name',
     list_per_page = 10
-    list_editable = 'first_name', 'last_name'
+    list_editable = 'first_name', 'last_name', 'show',
 
 
 @admin.register(models.Category)
