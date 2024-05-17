bad0229855aa91fb13b2a328a9621e99fca1bb3e
Rafael Mota Moraes <rafaelmota622@gmail.com>
2024-05-17 12:31:05 -0300

    Criando um formulário dinâmico com forms.ModelForm

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -2,12 +2,26 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.db.models import Q
 from contact.models import Contact
 from django.core.paginator import Paginator
+from django import forms
+
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = ('first_name', 'last_name', 'phone', )
 
 
 def create(request):
-    if request.method == 'POST':
-        ...
 
-    context = {}
+    if request.method == 'POST':
+        context = {
+            'form': ContactForm(data=request.POST)
+        }
+
+        return render(request, 'contact/create.html', context)
+
+    context = {
+        'form': ContactForm()
+    }
 
     return render(request, 'contact/create.html', context)
