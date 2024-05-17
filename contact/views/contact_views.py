76c82b4e28a7a6b746ae8624ff20c44699cae002
Rafael Mota Moraes <rafaelmota622@gmail.com>
2024-05-17 10:19:57 -0300

    Usando um padrão comum para URLs de CRUD

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.db.models import Q
 from contact.models import Contact
-from django.http import Http404
+from django.core.paginator import Paginator
 
 
 def index(request):
-    contacts = Contact.objects.filter(show=True).order_by('-id')[:10]
+    contacts = Contact.objects.filter(show=True).order_by('-id')
+
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
     context = {
-        'contacts': contacts,
+        'page_obj': page_obj,
         'site_title': 'Contatos - '
     }
 
@@ -30,8 +35,13 @@ def search(request):
         Q(phone__icontains=search_value) |
         Q(email__icontains=search_value)
     ).order_by('-id')
+
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
     context = {
-        'contacts': contacts,
+        'page_obj': page_obj,
         'site_title': 'Pesquisa - ',
         'search_value': search_value
     }
